5e320e4ef7ce28997a49bba62222eea4a676572d
phillip edwards <phillipedwards02@gmail.com>
2022-11-10 10:43:23 -0800

    reduce loop counter

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
+
 
 import { ServerlessComponent } from "./serverless";
 
 export = async () => {
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 2; i++) {
         new ServerlessComponent(`serverless-comp-${i}`, {
             tags: {
                 Project: pulumi.getProject(),
@@ -20,3 +21,20 @@ export = async () => {
         "HI": "HI"
     }
 };
+
+/*
+===============
+Pulumi Service Backend
+===============
+- pulumi up -f --non-interactive -p 250     :: 6:17
+- pulumi up -f --non-interactive -p 100     :: 6:31
+- pulumi up -f --non-interactive            :: 9:59
+
+===============
+Local Filestate Backend
+===============
+- pulumi up -f --non-interactive -p 250     :: 5:20
+- pulumi up -f --non-interactive -p 100     :: 5:22
+- pulumi up -f --non-interactive            :: 5:12
+
+*/
